5a91487b5f28b9e7b39e5685e7c79d203a61631f
Joseph Gumulka <josephgumulka@gmail.com>
2026-07-02 19:16:39 -0400

    Tested and confirmed all pumps operational.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,16 @@ void setup()
 {
     pinMode(POWER_LED, OUTPUT); //Setting mode to output for power LED
     digitalWrite(POWER_LED, HIGH);  //If there is power then power LED should always be on
+
+    pinMode(FILL_PUMP, OUTPUT); //Setting mode to output for pump fill
+    digitalWrite(FILL_PUMP, LOW); //Pump off as default
 }
 
 void loop()
 {
-   
+    digitalWrite(FILL_PUMP, HIGH);
+    delay(2000);
+
+    digitalWrite(FILL_PUMP, LOW);
+    delay(2000);
 }
