bb3b40dfbd00853f95b22aadbf6361fb1f262e31
Joseph Gumulka <josephgumulka@gmail.com>
2026-07-02 21:11:54 -0400

    Switch and init functions completed.

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,1 +1,36 @@
-//LOGIC TO BE IMPLIMENTED
+#include "states.h"
+
+state_machine::state_machine()  //Init function, idle state by default
+{
+    current_state = state::idle;
+}
+
+void state_machine::update() //Switch statement, seperated for ease of reading state functions
+{
+    switch (current_state)
+    {
+        case state::idle:
+            idle_state();
+            break;
+        case state::filling:
+            filling_state();
+            break;
+        case state::testing:
+            testing_state();
+            break;
+        case state::pass_drain:
+            pass_drainstate();
+            break;
+        case state::fail_drain:
+            fail_drainstate();
+            break;
+        default:
+            current_state = state::idle;
+            break;
+    }
+}
+
+state state_machine::getstate() const
+{
+    return current_state;
+}
