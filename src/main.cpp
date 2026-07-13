53319b42bd1d0ff65646e9da5413dfe94820cbcb
Joseph Gumulka <josephgumulka@gmail.com>
2026-07-13 18:48:46 -0400

    Completed and tested basic state machine function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,33 @@
-#include <HardwareSerial.h>
-const int TDS_PIN = 34;  // Change to your ADC pin
+#include <Arduino.h>;
 
-void setup() {
-  Serial.begin(115200);
+#include "pins.h";
+#include "config.h";
+#include "states.h";
+
+state_machine machine;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(POWER_LED, OUTPUT); //Setting mode to output for power LED
+    digitalWrite(POWER_LED, HIGH);  //If there is power then power LED should always be on
+
+    pinMode(PASS_LED, OUTPUT);
+    pinMode(FAIL_LED, OUTPUT);
+    pinMode(PROCESSING_LED, OUTPUT);
+
+    pinMode(FILL_PUMP, OUTPUT); //Pump pin set to output mode
+    pinMode(PASS_PUMP, OUTPUT);
+    pinMode(FAIL_PUMP, OUTPUT);
+    digitalWrite(PASS_LED, LOW);
+    digitalWrite(FAIL_LED, LOW);
+    digitalWrite(PROCESSING_LED, LOW);
+
+    pinMode(START_BUTTON, INPUT_PULLUP);
 }
 
-void loop() {
-  int raw = analogRead(TDS_PIN);
-
-  Serial.print("Raw ADC: ");
-  Serial.println(raw);
-
-  delay(500);
+void loop()
+{
+   machine.update();
 }
